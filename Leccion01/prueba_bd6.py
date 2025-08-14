9b839f6b6538b761fe6ec5e8fd8091ecc41305e7
Gadiel-Sosa <gadielantoniososachan@gmail.com>
2025-08-13 20:18:49 -0600

    actualizar registros de una bd

--- Leccion01/prueba_bd6.py
+++ Leccion01/prueba_bd6.py
@@ -1,5 +1,5 @@
 # ACTUALIZAR UN REGISTRO EN LA DB
-import psycopg
+import psycopg # type: ignore
 with psycopg.connect(
     user = 'postgres',
     password = 'admin',
@@ -8,4 +8,17 @@ with psycopg.connect(
     dbname = 'test_db'
 ) as conexion:
     with conexion.cursor() as cursor:
-        sentencia =''
+        sentencia ='UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona = %s'
+        sentencia2 = 'SELECT * FROM persona'
+        entrada = input('Ingresa los valores a modificar y el id_persona a quien se modifica los datos (nombre, apellido, email, id): ')
+        
+        valores = entrada.split(',')
+        valores[-1] = int(valores[-1])
+        valores = tuple(valores)
+        cursor.execute(sentencia,valores)
+        total_registros = cursor.rowcount
+        print(f'Total de registros modificados: {total_registros}\n')
+        cursor.execute(sentencia2)
+        registros = cursor.fetchall()
+        for registro in registros:
+            print(registro)
